16d0a464ec1c61da66a8bc8e5b8235f5fd98728e
Preston Wallace <wallace.preston@gmail.com>
2023-03-18 06:06:57 -0700

    updates syntax to use recommended setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import {
   createBrowserRouter,
-  createRoutesFromElements,
-  Route,
   RouterProvider,
+  // createRoutesFromElements,
+  // Route,
 } from 'react-router-dom';
 import { Home, PokemonDetails, Layout } from './routes';
 
 import { loader } from './routes/Home';
 import { loader as detailsLoader } from './routes/PokemonDetails';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route element={<Layout />}>
-      <Route path='/' element={<Home />} loader={loader}  />
-      <Route path='/:name' element={<PokemonDetails />} loader={detailsLoader} />
-    </Route>
-  )
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+        loader,
+      },
+      {
+        path: '/:name',
+        element: <PokemonDetails />,
+        loader: detailsLoader,
+      },
+    ],
+  },
+]);
+
+// // alternate syntax using JSX
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route element={<Layout />}>
+//       <Route path='/' element={<Home />} loader={loader}  />
+//       <Route path='/:name' element={<PokemonDetails />} loader={detailsLoader} />
+//     </Route>
+//   )
+// );
 
 function App() {
   return (
