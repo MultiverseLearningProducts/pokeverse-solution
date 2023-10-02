7f525e2121d6da601e799c148a2acc5f54d87aa0
stevenjlance <steven.j.lance@gmail.com>
2023-10-02 10:08:04 -0400

    updated week 4

--- src/App.test.js
+++ src/App.test.js
@@ -10,7 +10,24 @@ describe('App', () => {
       Promise.resolve({
         json: () =>
           Promise.resolve({
-            results: [],
+            results: [
+              {
+                "name": "bulbasaur",
+                "url": "https://pokeapi.co/api/v2/pokemon/1/"
+              },
+              {
+                "name": "ivysaur",
+                "url": "https://pokeapi.co/api/v2/pokemon/2/"
+              },
+              {
+                "name": "venusaur",
+                "url": "https://pokeapi.co/api/v2/pokemon/3/"
+              },
+              {
+                "name": "charmander",
+                "url": "https://pokeapi.co/api/v2/pokemon/4/"
+              }          
+            ],
           }),
       })
     );
@@ -24,6 +41,13 @@ describe('App', () => {
       render(<App />);
     });
     expect(screen.getByText("Pokeverse")).toBeTruthy();
+  });
+
+  it('pokemon cards are rendered', async () => {
+    await act(async () => {
+      render(<App />);
+    });
+    expect(screen.getByText("bulbasaur")).toBeTruthy();
   });
 
 
